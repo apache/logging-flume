10f91e143b91c53245b6c5022a0f61047ee519ab
Andrew Bayer <abayer@apache.org>
2011-08-02 16:04:03 +0000

    CDH-1372: Fix intermittent TestFlumeNode.testFlumeNodeReconnect failure
    From: Jonathan Hsieh <jon@cloudera.com>
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/flume/trunk@1155597 13f79535-47bb-0310-9956-ffa450edef68

--- src/javatest/com/cloudera/flume/agent/TestFlumeNode.java
+++ src/javatest/com/cloudera/flume/agent/TestFlumeNode.java
@@ -220,6 +220,11 @@ public class TestFlumeNode extends TestCase {
     }
   }
 
+  /**
+   * This test starts a node, then starts a master, and waits until a node shows
+   * up. Then it closes, down and does so again, demonstrating that a node will
+   * reconnect
+   */
   public void testFlumeNodeReconnect() throws TTransportException, IOException,
       InterruptedException {
 
@@ -241,7 +246,7 @@ public class TestFlumeNode extends TestCase {
     assertEquals(0, fm.getKnownNodes().size());
     fm.serve();
     LOG.info("flume master 1 open ");
-    while (fm.getKnownNodes().size() > 0) {
+    while (fm.getKnownNodes().size() == 0) {
       Clock.sleep(1000);
     }
     fm.shutdown();
@@ -253,7 +258,7 @@ public class TestFlumeNode extends TestCase {
     assertEquals(0, fm2.getKnownNodes().size());
     fm2.serve();
     LOG.info("flume master 2 open ");
-    while (fm2.getKnownNodes().size() > 0) {
+    while (fm2.getKnownNodes().size() == 0) {
       Clock.sleep(1000);
     }
     fm2.shutdown();
