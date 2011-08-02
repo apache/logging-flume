25a855c7c6003e2b1d0884122f313600d9090e5d
Andrew Bayer <abayer@apache.org>
2011-08-02 16:04:28 +0000

    FLUME-95: Check JVM version on service startup - currently enforces JVM 1.6+ and exits with error otherwise.
    From: vibhor <vibhor@ubuntu.ubuntu-domain>
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/flume/trunk@1155685 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/com/cloudera/flume/master/FlumeMaster.java
+++ src/java/com/cloudera/flume/master/FlumeMaster.java
@@ -45,6 +45,7 @@ import com.cloudera.flume.reporter.Reportable;
 import com.cloudera.flume.reporter.server.ReportServer;
 import com.cloudera.flume.util.FlumeVMInfo;
 import com.cloudera.flume.util.SystemInfo;
+import com.cloudera.util.CheckJavaVersion;
 import com.cloudera.util.NetUtils;
 import com.cloudera.util.StatusHttpServer;
 
@@ -79,7 +80,7 @@ public class FlumeMaster implements Reportable {
   final ConfigurationManager specman;
   final StatusManager statman;
   final MasterAckManager ackman;
-  
+
   final String uniqueMasterName;
 
   Thread reaper;
@@ -117,9 +118,9 @@ public class FlumeMaster implements Reportable {
   public FlumeMaster(FlumeConfiguration cfg, boolean doHttp) {
     this.cfg = cfg;
     instance = this;
-    
+
     this.uniqueMasterName = "flume-master-" + cfg.getMasterServerId();
-    
+
     this.doHttp = doHttp;
     this.cmdman = new CommandManager();
     ConfigStore cfgStore = createConfigStore(FlumeConfiguration.get());
@@ -294,7 +295,7 @@ public class FlumeMaster implements Reportable {
       if (cfg.getMasterStore().equals(ZK_CFG_STORE)) {
         ZooKeeperService.get().shutdown();
       }
-      
+
     } catch (IOException e) {
       LOG.error("Exception when shutting down master! " + e.getMessage());
       LOG.debug(e, e);
@@ -380,7 +381,12 @@ public class FlumeMaster implements Reportable {
   public static void main(String[] argv) {
     FlumeNode.logVersion(LOG, Level.INFO);
     FlumeNode.logEnvironment(LOG, Level.INFO);
-
+    // Make sure the Java version is not older than 1.6
+    if (CheckJavaVersion.checkVersion()) {
+      LOG
+          .error("Exitting because of an old Java version or Java version in bad format");
+      System.exit(-1);
+    }
     FlumeConfiguration.hardExitLoadConfig(); // if config file is bad hardexit.
 
     CommandLine cmd = null;
