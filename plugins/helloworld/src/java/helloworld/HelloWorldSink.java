ddd3f1dae606703e9c4c1ee5bdb979c149435859
Andrew Bayer <abayer@apache.org>
2011-08-02 16:04:16 +0000

    FLUME-101: update plugin docs and provide a working example
    From: phunt <phunt@cloudera.com>
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/flume/trunk@1155646 13f79535-47bb-0310-9956-ffa450edef68

---
+++ plugins/helloworld/src/java/helloworld/HelloWorldSink.java
@@ -0,0 +1,87 @@
+/**
+ * Licensed to Cloudera, Inc. under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  Cloudera, Inc. licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package helloworld;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.cloudera.flume.conf.Context;
+import com.cloudera.flume.conf.SinkFactory.SinkBuilder;
+import com.cloudera.flume.core.Event;
+import com.cloudera.flume.core.EventSink;
+import com.cloudera.util.Pair;
+import com.google.common.base.Preconditions;
+
+/**
+ * Simple Sink that writes to a "helloworld.txt" file.
+ */
+public class HelloWorldSink extends EventSink.Base {
+  static Logger LOG = Logger.getLogger(HelloWorldSink.class);
+  private PrintWriter pw;
+  
+  @Override
+  public void open() throws IOException {
+    // Initialized the sink
+    pw = new PrintWriter(new FileWriter("helloworld.txt"));
+  }
+
+  @Override
+  public void append(Event e) throws IOException {
+    // append the event to the output
+    
+    // here we are assuming the body is a string
+    pw.println(new String(e.getBody()));
+    pw.flush(); // so we can see it in the file right away
+  }
+
+  @Override
+  public void close() throws IOException {
+    // Cleanup
+    pw.flush();
+    pw.close();
+  }
+
+  public static SinkBuilder builder() {
+    return new SinkBuilder() {
+      // construct a new parameterized sink
+      @Override
+      public EventSink build(Context context, String... argv) {
+        Preconditions.checkArgument(argv.length == 0,
+            "usage: helloWorldSink");
+
+        return new HelloWorldSink();
+      }
+    };
+  }
+
+  /**
+   * This is a special function used by the SourceFactory to pull in this class
+   * as a plugin sink.
+   */
+  public static List<Pair<String, SinkBuilder>> getSinkBuilders() {
+    List<Pair<String, SinkBuilder>> builders =
+      new ArrayList<Pair<String, SinkBuilder>>();
+    builders.add(new Pair<String, SinkBuilder>("helloWorldSink", builder()));
+    return builders;
+  }
+}
---
+++ plugins/helloworld/src/java/helloworld/HelloWorldSource.java
@@ -0,0 +1,87 @@
+/**
+ * Licensed to Cloudera, Inc. under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  Cloudera, Inc. licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package helloworld;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.cloudera.flume.conf.SourceFactory.SourceBuilder;
+import com.cloudera.flume.core.Event;
+import com.cloudera.flume.core.EventImpl;
+import com.cloudera.flume.core.EventSource;
+import com.cloudera.util.Pair;
+import com.google.common.base.Preconditions;
+
+/**
+ * Simple Source that generates a "hello world!" event every 3 seconds.
+ */
+public class HelloWorldSource extends EventSource.Base {
+  static Logger LOG = Logger.getLogger(HelloWorldSource.class);
+  
+  private String helloWorld;
+
+  @Override
+  public void open() throws IOException {
+    // Initialized the source
+    helloWorld = "Hello World!";
+  }
+
+  @Override
+  public Event next() throws IOException {
+    // Next returns the next event, blocking if none available.
+    try {
+      Thread.sleep(3000);
+    } catch (InterruptedException e) {
+      throw new IOException(e.getMessage(), e);
+    }
+    return new EventImpl(helloWorld.getBytes());
+  }
+
+  @Override
+  public void close() throws IOException {
+    // Cleanup
+    helloWorld = null;
+  }
+
+  public static SourceBuilder builder() {
+    // construct a new parameterized source
+    return new SourceBuilder() {
+      @Override
+      public EventSource build(String... argv) {
+        Preconditions.checkArgument(argv.length == 0,
+            "usage: helloWorldSource");
+
+        return new HelloWorldSource();
+      }
+    };
+  }
+
+  /**
+   * This is a special function used by the SourceFactory to pull in this class
+   * as a plugin source.
+   */
+  public static List<Pair<String, SourceBuilder>> getSourceBuilders() {
+    List<Pair<String, SourceBuilder>> builders =
+      new ArrayList<Pair<String, SourceBuilder>>();
+    builders.add(new Pair<String, SourceBuilder>("helloWorldSource", builder()));
+    return builders;
+  }
+}
