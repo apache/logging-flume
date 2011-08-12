4c29fefccc6837e564a1632f378cc9222e8f54eb
Eric Sammer <esammer@apache.org>
2011-08-12 00:46:35 +0000

    - Made all CounterGroup methods synchronized.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/flume/branches/flume-728@1156872 13f79535-47bb-0310-9956-ffa450edef68

--- flume-ng-core/src/main/java/org/apache/flume/core/CounterGroup.java
+++ flume-ng-core/src/main/java/org/apache/flume/core/CounterGroup.java
@@ -1,57 +1,55 @@
 package org.apache.flume.core;
 
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class CounterGroup {
 
   private String name;
-  private ConcurrentHashMap<String, AtomicLong> counters;
+  private HashMap<String, AtomicLong> counters;
 
   public CounterGroup() {
-    counters = new ConcurrentHashMap<String, AtomicLong>();
+    counters = new HashMap<String, AtomicLong>();
   }
 
-  public Long get(String name) {
+  public synchronized Long get(String name) {
     return getCounter(name).get();
   }
 
-  public Long incrementAndGet(String name) {
+  public synchronized Long incrementAndGet(String name) {
     return getCounter(name).incrementAndGet();
   }
 
-  public Long addAndGet(String name, Long delta) {
+  public synchronized Long addAndGet(String name, Long delta) {
     return getCounter(name).addAndGet(delta);
   }
 
-  public AtomicLong getCounter(String name) {
-    synchronized (counters) {
-      if (!counters.containsKey(name)) {
-        counters.put(name, new AtomicLong());
-      }
+  public synchronized AtomicLong getCounter(String name) {
+    if (!counters.containsKey(name)) {
+      counters.put(name, new AtomicLong());
     }
 
     return counters.get(name);
   }
 
   @Override
-  public String toString() {
+  public synchronized String toString() {
     return "{ name:" + name + " counters:" + counters + " }";
   }
 
-  public String getName() {
+  public synchronized String getName() {
     return name;
   }
 
-  public void setName(String name) {
+  public synchronized void setName(String name) {
     this.name = name;
   }
 
-  public ConcurrentHashMap<String, AtomicLong> getCounters() {
+  public synchronized HashMap<String, AtomicLong> getCounters() {
     return counters;
   }
 
-  public void setCounters(ConcurrentHashMap<String, AtomicLong> counters) {
+  public synchronized void setCounters(HashMap<String, AtomicLong> counters) {
     this.counters = counters;
   }
 
