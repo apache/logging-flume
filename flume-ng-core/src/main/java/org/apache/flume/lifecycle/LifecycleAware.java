962d59f1637810c75b0e23cb8518e0a1bfd7f097
Eric Sammer <esammer@apache.org>
2011-08-12 00:47:06 +0000

    - Added javadoc.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/flume/branches/flume-728@1156883 13f79535-47bb-0310-9956-ffa450edef68

--- flume-ng-core/src/main/java/org/apache/flume/lifecycle/LifecycleAware.java
+++ flume-ng-core/src/main/java/org/apache/flume/lifecycle/LifecycleAware.java
@@ -2,14 +2,105 @@ package org.apache.flume.lifecycle;
 
 import org.apache.flume.core.Context;
 
+/**
+ * <p>
+ * An interface implemented by any class that has a defined, stateful,
+ * lifecycle.
+ * </p>
+ * <p>
+ * Implementations of {@link LifecycleAware} conform to a standard method of
+ * starting, stopping, and reporting their current state. Additionally, this
+ * interface creates a standard method of communicating failure to perform a
+ * lifecycle operation to the caller (i.e. via {@link LifecycleException}). It
+ * is never considered valid to call {@link #start(Context)} or
+ * {@link #stop(Context)} more than once or to call them in the wrong order.
+ * While this is not strictly enforced, it may be in the future.
+ * </p>
+ * <p>
+ * Example services may include Flume nodes and the master, but also lower level
+ * components that can be controlled in a similar manner.
+ * </p>
+ * <p>
+ * Example usage
+ * </p>
+ * <code>
+ *  public class MyService implements LifecycleAware {
+ * 
+ *    private LifecycleState lifecycleState;
+ * 
+ *    public MyService() {
+ *      lifecycleState = LifecycleState.IDLE;
+ *    }
+ * 
+ *    @Override
+ *    public void start(Context context) throws LifecycleException,
+ *      InterruptedException {
+ * 
+ *      ...your code does something.
+ * 
+ *      lifecycleState = LifecycleState.START;
+ *    }
+ * 
+ *    @Override
+ *    public void stop(Context context) throws LifecycleException,
+ *      InterruptedException {
+ * 
+ *      try {
+ *        ...you stop services here.
+ *      } catch (SomethingException) {
+ *        lifecycleState = LifecycleState.ERROR;
+ *      }
+ * 
+ *      lifecycleState = LifecycleState.STOP;
+ *    }
+ * 
+ *    @Override
+ *    public LifecycleState getLifecycleState() {
+ *      return lifecycleState;
+ *    }
+ * 
+ *  }
+ * </code>
+ */
 public interface LifecycleAware {
 
+  /**
+   * <p>
+   * Starts a service or component.
+   * </p>
+   * <p>
+   * Implementations should determine the result of any start logic and effect
+   * the return value of {@link #getLifecycleState()} accordingly.
+   * </p>
+   * 
+   * @param context
+   * @throws LifecycleException
+   * @throws InterruptedException
+   */
   public void start(Context context) throws LifecycleException,
       InterruptedException;
 
+  /**
+   * <p>
+   * Stops a service or component.
+   * </p>
+   * <p>
+   * Implementations should determine the result of any stop logic and effect
+   * the return value of {@link #getLifecycleState()} accordingly.
+   * </p>
+   * 
+   * @param context
+   * @throws LifecycleException
+   * @throws InterruptedException
+   */
   public void stop(Context context) throws LifecycleException,
       InterruptedException;
 
+  /**
+   * <p>
+   * Return the current state of the service or component.
+   * </p>
+   */
   public LifecycleState getLifecycleState();
 
 }
