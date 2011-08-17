8f21f9a7b254ad92e3aac2dfe88c6bba91c8cc41
Eric Sammer <esammer@apache.org>
2011-08-17 20:55:08 +0000

    - Stupidly simple test of flume node main().
    git-svn-id: https://svn.apache.org/repos/asf/incubator/flume/branches/flume-728@1158900 13f79535-47bb-0310-9956-ffa450edef68

---
+++ flume-ng-node/src/test/java/org/apache/flume/node/TestFlumeNodeApplication.java
@@ -0,0 +1,14 @@
+package org.apache.flume.node;
+
+import org.junit.Test;
+
+public class TestFlumeNodeApplication {
+
+  @Test
+  public void testApplication() {
+    String[] args = new String[] {};
+
+    Application.main(args);
+  }
+
+}
