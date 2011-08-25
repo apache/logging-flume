c9db94fc17c9b440469498867c15a6796c239d0f
Eric Sammer <esammer@apache.org>
2011-08-25 18:49:38 +0000

    - Added missing exceptions from throws clause of open / close methods.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/flume/branches/flume-728@1161685 13f79535-47bb-0310-9956-ffa450edef68

--- flume-ng-core/src/main/java/org/apache/flume/source/AbstractEventSource.java
+++ flume-ng-core/src/main/java/org/apache/flume/source/AbstractEventSource.java
@@ -4,11 +4,13 @@ import org.apache.flume.Context;
 import org.apache.flume.Event;
 import org.apache.flume.EventDeliveryException;
 import org.apache.flume.EventSource;
+import org.apache.flume.lifecycle.LifecycleException;
 
 abstract public class AbstractEventSource implements EventSource {
 
   @Override
-  public void open(Context context) throws InterruptedException {
+  public void open(Context context) throws LifecycleException,
+      InterruptedException {
     // Empty implementation by default.
   }
 
@@ -17,7 +19,8 @@ abstract public class AbstractEventSource implements EventSource {
       EventDeliveryException;
 
   @Override
-  public void close(Context context) throws InterruptedException {
+  public void close(Context context) throws LifecycleException,
+      InterruptedException {
     // Empty implementation by default.
   }
 
