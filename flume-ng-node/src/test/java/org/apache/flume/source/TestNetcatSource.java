bd07818f9357dc4772333a2b12872c14f8a360f6
Eric Sammer <esammer@apache.org>
2011-08-25 18:50:14 +0000

    - Added / improved tests of NetcatSource including configuration via Configurable and   the new WAL support. This is a work in progress.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/flume/branches/flume-728@1161691 13f79535-47bb-0310-9956-ffa450edef68

--- flume-ng-node/src/test/java/org/apache/flume/source/TestNetcatSource.java
+++ flume-ng-node/src/test/java/org/apache/flume/source/TestNetcatSource.java
@@ -1,5 +1,6 @@
 package org.apache.flume.source;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.Writer;
 import java.net.InetSocketAddress;
@@ -9,10 +10,13 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.flume.Context;
 import org.apache.flume.Event;
 import org.apache.flume.EventDeliveryException;
 import org.apache.flume.EventSource;
+import org.apache.flume.conf.Configurables;
+import org.apache.flume.durability.file.FileBasedWALManager;
 import org.apache.flume.lifecycle.LifecycleException;
 import org.junit.Assert;
 import org.junit.Before;
@@ -35,7 +39,10 @@ public class TestNetcatSource {
     Context context = new Context();
 
     /* FIXME: Use a random port for testing. */
-    ((NetcatSource) source).setPort(41414);
+    context.put("logicalNode.name", "test");
+    context.put("source.port", 41414);
+
+    Configurables.configure(source, context);
 
     source.open(context);
 
@@ -77,6 +84,75 @@ public class TestNetcatSource {
     }
 
     source.close(context);
+  }
+
+  @Test
+  public void testDurability() throws InterruptedException, LifecycleException,
+      EventDeliveryException, IOException {
+
+    /* FIXME: Use a random port for testing. */
+    ((NetcatSource) source).setPort(41414);
+
+    FileBasedWALManager walManager = new FileBasedWALManager();
+
+    walManager.setDirectory(new File("/tmp/flume-ncs-tests", "wal-test"));
+    walManager.getDirectory().mkdirs();
+
+    ((NetcatSource) source).setWALManager(walManager);
+
+    ExecutorService executor = Executors.newFixedThreadPool(3);
+    Context context = new Context();
+
+    context.put("logicalNode.name", "test");
+    context.put("source.port", 41414);
+
+    Configurables.configure(source, context);
+
+    source.open(context);
+
+    Runnable clientRequestRunnable = new Runnable() {
+
+      @Override
+      public void run() {
+        try {
+          SocketChannel clientChannel = SocketChannel
+              .open(new InetSocketAddress(41414));
+
+          Writer writer = Channels.newWriter(clientChannel, "utf-8");
+
+          writer.write("Test message");
+
+          writer.flush();
+          clientChannel.close();
+        } catch (IOException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+      }
+
+    };
+
+    for (int i = 0; i < 100; i++) {
+      executor.submit(clientRequestRunnable);
+
+      Event event = source.next(context);
+
+      Assert.assertNotNull(event);
+      Assert.assertArrayEquals("Test message".getBytes(), event.getBody());
+    }
+
+    executor.shutdown();
+
+    while (!executor.isTerminated()) {
+      executor.awaitTermination(500, TimeUnit.MILLISECONDS);
+    }
+
+    source.close(context);
+
+    FileUtils.deleteDirectory(walManager.getDirectory());
+
+    /* Only delete the parent if it's empty. */
+    walManager.getDirectory().getParentFile().delete();
   }
 
 }
