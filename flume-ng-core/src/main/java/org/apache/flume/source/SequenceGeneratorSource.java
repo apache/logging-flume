424a4d89bf067d1af6875ee6649424eee18a994a
Eric Sammer <esammer@apache.org>
2011-09-23 14:06:56 +0000

    - Added logging and metrics counters to Seq source.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/flume/branches/flume-728@1174782 13f79535-47bb-0310-9956-ffa450edef68

--- flume-ng-core/src/main/java/org/apache/flume/source/SequenceGeneratorSource.java
+++ flume-ng-core/src/main/java/org/apache/flume/source/SequenceGeneratorSource.java
@@ -1,18 +1,26 @@
 package org.apache.flume.source;
 
 import org.apache.flume.Channel;
+import org.apache.flume.CounterGroup;
 import org.apache.flume.EventDeliveryException;
 import org.apache.flume.PollableSource;
 import org.apache.flume.Transaction;
 import org.apache.flume.event.EventBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SequenceGeneratorSource extends AbstractSource implements
     PollableSource {
 
+  private static final Logger logger = LoggerFactory
+      .getLogger(SequenceGeneratorSource.class);
+
   private long sequence;
+  private CounterGroup counterGroup;
 
   public SequenceGeneratorSource() {
     sequence = 0;
+    counterGroup = new CounterGroup();
   }
 
   @Override
@@ -24,13 +32,34 @@ public class SequenceGeneratorSource extends AbstractSource implements
       transaction.begin();
       channel.put(EventBuilder.withBody(String.valueOf(sequence++).getBytes()));
       transaction.commit();
+
+      counterGroup.incrementAndGet("events.successful");
     } catch (Exception e) {
       transaction.rollback();
+      counterGroup.incrementAndGet("events.failed");
     } finally {
       transaction.close();
     }
 
     return Status.READY;
+  }
+
+  @Override
+  public void start() {
+    logger.info("Sequence generator source starting");
+
+    super.start();
+
+    logger.debug("Sequence generator source started");
+  }
+
+  @Override
+  public void stop() {
+    logger.info("Sequence generator source stopping");
+
+    super.stop();
+
+    logger.info("Sequence generator source stopped. Metrics:{}", counterGroup);
   }
 
 }
