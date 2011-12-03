6a0e62ada53e1c252c3d2def43e5296099e4b82a
Eric Sammer <esammer@apache.org>
2011-12-03 00:33:35 +0000

    FLUME-866: Remove old plugins and log4j appender
    git-svn-id: https://svn.apache.org/repos/asf/incubator/flume/branches/flume-728@1209799 13f79535-47bb-0310-9956-ffa450edef68

--- flume-ng-core/src/test/java/org/apache/flume/channel/TestFanoutChannel.java
+++ flume-ng-core/src/test/java/org/apache/flume/channel/TestFanoutChannel.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.flume.channel;
 
 import org.apache.flume.Channel;
@@ -6,8 +25,6 @@ import org.apache.flume.Context;
 import org.apache.flume.Event;
 import org.apache.flume.EventDeliveryException;
 import org.apache.flume.Transaction;
-import org.apache.flume.channel.MemoryChannel;
-import org.apache.flume.channel.FanoutChannel.wrapperTransaction;
 import org.apache.flume.conf.Configurables;
 import org.apache.flume.event.EventBuilder;
 import org.junit.Assert;
