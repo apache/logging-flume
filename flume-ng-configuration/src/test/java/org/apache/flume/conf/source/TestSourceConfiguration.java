f32e8cd4c529b1b334d8adf0282647eac3472294
Hari Shreedharan <harishreedharan@gmail.com>
2013-01-25 13:30:13 -0800

    FLUME-1847. NPE in SourceConfiguration
    (Brock Noland via Hari Shreedharan)

---
+++ flume-ng-configuration/src/test/java/org/apache/flume/conf/source/TestSourceConfiguration.java
@@ -0,0 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.flume.conf.source;
+
+import org.apache.flume.Context;
+import org.apache.flume.conf.ConfigurationException;
+import org.junit.Test;
+
+public class TestSourceConfiguration {
+
+  /**
+   * Test fails without FLUME-1847
+   */
+  @Test(expected = ConfigurationException.class)
+  public void testFLUME1847() throws Exception {
+    Context context = new Context();
+    context.put("type", "something");
+    SourceConfiguration sourceConfig = new SourceConfiguration("src");
+    sourceConfig.configure(context);
+
+  }
+}
