e1cb6913f358e4de54d69d1f6f21edecf06a9da2
Hari Shreedharan <hshreedharan@apache.org>
2014-10-03 11:52:13 -0700

    FLUME-2479. Kafka property auto.commit.enable is incorrect for KafkaSource.
    (Gwen Shapira via Hari)

--- flume-ng-sources/flume-kafka-source/src/main/java/org/apache/flume/source/kafka/KafkaSourceConstants.java
+++ flume-ng-sources/flume-kafka-source/src/main/java/org/apache/flume/source/kafka/KafkaSourceConstants.java
@@ -22,7 +22,7 @@ public class KafkaSourceConstants {
   public static final String BATCH_SIZE = "batchSize";
   public static final String BATCH_DURATION_MS = "batchDurationMillis";
   public static final String CONSUMER_TIMEOUT = "consumer.timeout.ms";
-  public static final String AUTO_COMMIT_ENABLED = "auto.commit.enabled";
+  public static final String AUTO_COMMIT_ENABLED = "auto.commit.enable";
   public static final String ZOOKEEPER_CONNECT = "zookeeper.connect";
   public static final String ZOOKEEPER_CONNECT_FLUME = "zookeeperConnect";
   public static final String GROUP_ID = "group.id";
@@ -33,7 +33,7 @@ public class KafkaSourceConstants {
   public static final int DEFAULT_BATCH_SIZE = 1000;
   public static final int DEFAULT_BATCH_DURATION = 1000;
   public static final String DEFAULT_CONSUMER_TIMEOUT = "10";
-  public static final boolean DEFAULT_AUTO_COMMIT =  false;
+  public static final String DEFAULT_AUTO_COMMIT =  "false";
   public static final String DEFAULT_GROUP_ID = "flume";
 
 }
