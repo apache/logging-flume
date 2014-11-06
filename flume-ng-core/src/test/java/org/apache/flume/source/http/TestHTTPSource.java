ab00f5c5d4ab8c3adccbc1ee296a18b2bfa412d3
Jarek Jarcec Cecho <jarcec@apache.org>
2014-11-05 16:53:13 -0800

    FLUME-2533: HTTPS tests fail on Java 6
    (Hari Shreedharan via Jarek Jarcec Cecho)

--- flume-ng-core/src/test/java/org/apache/flume/source/http/TestHTTPSource.java
+++ flume-ng-core/src/test/java/org/apache/flume/source/http/TestHTTPSource.java
@@ -367,6 +367,7 @@ public class TestHTTPSource {
       }};
 
       SSLContext sc = null;
+      javax.net.ssl.SSLSocketFactory factory = null;
       if (System.getProperty("java.vendor").contains("IBM")) {
         sc = SSLContext.getInstance("SSL_TLS");
       } else {
@@ -379,11 +380,11 @@ public class TestHTTPSource {
         }
       };
       sc.init(null, trustAllCerts, new SecureRandom());
-      javax.net.ssl.SSLSocketFactory factory = null;
+
       if(protocol != null) {
-        factory = new TestSSLSocketFactory(sc.getSocketFactory(), protocol);
+        factory = new DisabledProtocolsSocketFactory(sc.getSocketFactory(), protocol);
       } else {
-        factory = sc.getSocketFactory();
+        factory = new EnabledProtocolsSocketFactory(sc.getSocketFactory());
       }
       HttpsURLConnection.setDefaultSSLSocketFactory(factory);
       HttpsURLConnection.setDefaultHostnameVerifier(
@@ -490,21 +491,85 @@ public class TestHTTPSource {
     }
   }
 
-  private class TestSSLSocketFactory extends javax.net.ssl.SSLSocketFactory {
+  private class DisabledProtocolsSocketFactory extends javax.net.ssl.SSLSocketFactory {
 
     private final javax.net.ssl.SSLSocketFactory socketFactory;
     private final String[] protocols;
 
-    TestSSLSocketFactory(javax.net.ssl.SSLSocketFactory factory, String protocol) {
+    DisabledProtocolsSocketFactory(javax.net.ssl.SSLSocketFactory factory, String protocol) {
       this.socketFactory = factory;
       if(protocol.equals("SSLv2Hello")) {
         protocols = new String[2];
-        protocols[0] = "TLSv1.2";
+        protocols[0] = "TLSv1";
         protocols[1] = protocol;
       } else {
         protocols = new String[1];
         protocols[0] = protocol;
       }
+    }
+
+    @Override
+    public String[] getDefaultCipherSuites() {
+      return socketFactory.getDefaultCipherSuites();
+    }
+
+    @Override
+    public String[] getSupportedCipherSuites() {
+      return socketFactory.getSupportedCipherSuites();
+    }
+
+    @Override
+    public Socket createSocket(Socket socket, String s, int i, boolean b)
+      throws IOException {
+      SSLSocket sc = (SSLSocket) socketFactory.createSocket(socket, s, i, b);
+      sc.setEnabledProtocols(protocols);
+      return sc;
+    }
+
+    @Override
+    public Socket createSocket(String s, int i)
+      throws IOException, UnknownHostException {
+      SSLSocket sc = (SSLSocket)socketFactory.createSocket(s, i);
+      sc.setEnabledProtocols(protocols);
+      return sc;
+    }
+
+    @Override
+    public Socket createSocket(String s, int i, InetAddress inetAddress, int i2)
+      throws IOException, UnknownHostException {
+      SSLSocket sc = (SSLSocket)socketFactory.createSocket(s, i, inetAddress,
+        i2);
+      sc.setEnabledProtocols(protocols);
+      return sc;
+    }
+
+    @Override
+    public Socket createSocket(InetAddress inetAddress, int i)
+      throws IOException {
+      SSLSocket sc = (SSLSocket)socketFactory.createSocket(inetAddress, i);
+      sc.setEnabledProtocols(protocols);
+      return sc;
+    }
+
+    @Override
+    public Socket createSocket(InetAddress inetAddress, int i,
+      InetAddress inetAddress2, int i2) throws IOException {
+      SSLSocket sc = (SSLSocket)socketFactory.createSocket(inetAddress, i,
+        inetAddress2, i2);
+      sc.setEnabledProtocols(protocols);
+      return sc;
+    }
+  }
+
+  private class EnabledProtocolsSocketFactory extends javax.net.ssl.SSLSocketFactory {
+
+    private final javax.net.ssl.SSLSocketFactory socketFactory;
+    private final String[] protocols;
+
+    EnabledProtocolsSocketFactory(javax.net.ssl.SSLSocketFactory factory) {
+      this.socketFactory = factory;
+      protocols = new String[1];
+      protocols[0] = "TLSv1";
     }
 
     @Override
