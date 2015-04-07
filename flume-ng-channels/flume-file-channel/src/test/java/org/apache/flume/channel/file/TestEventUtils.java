6019fcf4c9f773b521813145e026c10c96584527
Hari Shreedharan <hshreedharan@apache.org>
2015-04-07 14:46:10 -0700

    FLUME-2613. Add support in FileChannelIntegrityTool to remove invalid events from the channel.
    (Ashish Paliwal via Hari)

---
+++ flume-ng-channels/flume-file-channel/src/test/java/org/apache/flume/channel/file/TestEventUtils.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.flume.channel.file;
+
+import junit.framework.Assert;
+import org.apache.flume.Event;
+import org.junit.Test;
+
+public class TestEventUtils {
+
+  @Test
+  public void testPutEvent() {
+    FlumeEvent event = new FlumeEvent(null, new byte[5]);
+    Put put = new Put(1l, 1l, event);
+    Event returnEvent = EventUtils.getEventFromTransactionEvent(put);
+    Assert.assertNotNull(returnEvent);
+    Assert.assertEquals(5, returnEvent.getBody().length);
+  }
+
+  @Test
+  public void testInvalidEvent() {
+    Take take = new Take(1l, 1l);
+    Event returnEvent = EventUtils.getEventFromTransactionEvent(take);
+    Assert.assertNull(returnEvent);
+  }
+
+}
--- flume-tools/src/test/java/org/apache/flume/tools/TestFileChannelIntegrityTool.java
+++ flume-tools/src/test/java/org/apache/flume/tools/TestFileChannelIntegrityTool.java
@@ -58,6 +58,7 @@ public class TestFileChannelIntegrityTool {
   private File checkpointDir;
   private File dataDir;
 
+  private static int invalidEvent = 0;
 
   @BeforeClass
   public static void setUpClass() throws Exception{
@@ -94,6 +95,45 @@ public class TestFileChannelIntegrityTool {
   public static void tearDownClass() throws Exception {
     FileUtils.deleteDirectory(origCheckpointDir);
     FileUtils.deleteDirectory(origDataDir);
+  }
+
+  @Test
+  public void testFixInvalidRecords() throws Exception {
+    doTestFixInvalidEvents(false, DummyEventVerifier.Builder.class.getName());
+  }
+  @Test
+  public void testFixInvalidRecordsWithCheckpoint() throws Exception {
+    doTestFixInvalidEvents(true, DummyEventVerifier.Builder.class.getName());
+  }
+
+  public void doTestFixInvalidEvents(boolean withCheckpoint, String eventHandler) throws Exception {
+    FileChannelIntegrityTool tool = new FileChannelIntegrityTool();
+    tool.run(new String[] {"-l", dataDir.toString(), "-e", eventHandler, "-DvalidatorValue=0"});
+    FileChannel channel = new FileChannel();
+    channel.setName("channel");
+    String cp;
+    if(withCheckpoint) {
+      cp = origCheckpointDir.toString();
+    } else {
+      FileUtils.deleteDirectory(checkpointDir);
+      Assert.assertTrue(checkpointDir.mkdirs());
+      cp = checkpointDir.toString();
+    }
+    ctx.put(FileChannelConfiguration.CHECKPOINT_DIR,cp);
+    ctx.put(FileChannelConfiguration.DATA_DIRS, dataDir.toString());
+    channel.configure(ctx);
+    channel.start();
+    Transaction tx = channel.getTransaction();
+    tx.begin();
+    int i = 0;
+    while(channel.take() != null) {
+      i++;
+    }
+    tx.commit();
+    tx.close();
+    channel.stop();
+    Assert.assertTrue(invalidEvent != 0);
+    Assert.assertEquals(25 - invalidEvent, i);
   }
 
   @Test
@@ -226,6 +266,12 @@ public class TestFileChannelIntegrityTool {
       Transaction tx = channel.getTransaction();
       tx.begin();
       for (int i = 0; i < 5; i++) {
+        if(i % 3 == 0) {
+          event.getBody()[0] = 0;
+          invalidEvent++;
+        } else {
+          event.getBody()[0] = 1;
+        }
         channel.put(event);
       }
       tx.commit();
@@ -243,5 +289,34 @@ public class TestFileChannelIntegrityTool {
         .in(log)
         .invoke(true));
     channel.stop();
+  }
+
+  public static class DummyEventVerifier implements EventValidator {
+
+    private int value = 0;
+
+    private DummyEventVerifier(int val) {
+      value = val;
+    }
+
+    @Override
+    public boolean validateEvent(Event event) {
+      return event.getBody()[0] != value;
+    }
+
+    public static class Builder implements EventValidator.Builder {
+
+      private int binaryValidator = 0;
+
+      @Override
+      public EventValidator build() {
+        return new DummyEventVerifier(binaryValidator);
+      }
+
+      @Override
+      public void configure(Context context) {
+        binaryValidator = context.getInteger("validatorValue");
+      }
+    }
   }
 }
