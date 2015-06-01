aa1fe5d25aa79b42a315b501608a1ed77bd47878
Zhiguo.Chen <me@chenzhiguo.cn>
2015-06-01 21:18:58 +0800

    Fix Configuration failed bug.

--- flume-ng-sinks/flume-ng-elasticsearch-sink/src/main/java/org/apache/flume/sink/elasticsearch/ElasticSearchSink.java
+++ flume-ng-sinks/flume-ng-elasticsearch-sink/src/main/java/org/apache/flume/sink/elasticsearch/ElasticSearchSink.java
@@ -313,7 +313,7 @@ public class ElasticSearchSink extends AbstractSink implements Configurable {
     }
 
     Context indexnameBuilderContext = new Context();
-    serializerContext.putAll(
+    indexnameBuilderContext.putAll(
             context.getSubProperties(INDEX_NAME_BUILDER_PREFIX));
 
     try {
