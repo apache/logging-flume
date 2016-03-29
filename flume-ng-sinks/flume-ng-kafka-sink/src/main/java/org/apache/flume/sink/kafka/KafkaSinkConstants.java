4a9f27d8c17271d4f513f05d2d9f7c21dfb883a0
Jarek Jarcec Cecho <jarcec@apache.org>
2016-03-29 09:43:40 -0700

    FLUME-2822: Flume-Kafka-Sink with new Producer
    (Jeff Holoman via Jarek Jarcec Cecho)

--- flume-ng-sinks/flume-ng-kafka-sink/src/main/java/org/apache/flume/sink/kafka/KafkaSinkConstants.java
+++ flume-ng-sinks/flume-ng-kafka-sink/src/main/java/org/apache/flume/sink/kafka/KafkaSinkConstants.java
@@ -18,30 +18,42 @@
 
 package org.apache.flume.sink.kafka;
 
-import kafka.serializer.StringDecoder;
+import org.apache.kafka.clients.CommonClientConfigs;
 
 public class KafkaSinkConstants {
 
-  public static final String PROPERTY_PREFIX = "kafka.";
+  public static final String KAFKA_PREFIX = "kafka.";
+  public static final String KAFKA_PRODUCER_PREFIX = KAFKA_PREFIX + "producer.";
 
   /* Properties */
 
-  public static final String TOPIC = "topic";
-  public static final String BATCH_SIZE = "batchSize";
+  public static final String TOPIC_CONFIG = KAFKA_PREFIX + "topic";
+  public static final String BATCH_SIZE = "flumeBatchSize";
+  public static final String BOOTSTRAP_SERVERS_CONFIG = KAFKA_PREFIX + CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG;
+
+  public static final String KEY_HEADER = "key";
+  public static final String TOPIC_HEADER = "topic";
+
+  public static final String DEFAULT_KEY_SERIALIZER = "org.apache.kafka.common.serialization.StringSerializer";
+  public static final String DEFAULT_VALUE_SERIAIZER = "org.apache.kafka.common.serialization.ByteArraySerializer";
+
+  public static final int DEFAULT_BATCH_SIZE = 100;
+  public static final String DEFAULT_TOPIC = "default-flume-topic";
+  public static final String DEFAULT_ACKS = "1";
+
+
+
+  /* Old Properties */
+
+   /* Properties */
+
+  public static final String OLD_BATCH_SIZE = "batchSize";
   public static final String MESSAGE_SERIALIZER_KEY = "serializer.class";
   public static final String KEY_SERIALIZER_KEY = "key.serializer.class";
-  public static final String BROKER_LIST_KEY = "metadata.broker.list";
-  public static final String REQUIRED_ACKS_KEY = "request.required.acks";
   public static final String BROKER_LIST_FLUME_KEY = "brokerList";
   public static final String REQUIRED_ACKS_FLUME_KEY = "requiredAcks";
 
 
 
-  public static final int DEFAULT_BATCH_SIZE = 100;
-  public static final String DEFAULT_TOPIC = "default-flume-topic";
-  public static final String DEFAULT_MESSAGE_SERIALIZER =
-          "kafka.serializer.DefaultEncoder";
-  public static final String DEFAULT_KEY_SERIALIZER =
-          "kafka.serializer.StringEncoder";
-  public static final String DEFAULT_REQUIRED_ACKS = "1";
 }
+
