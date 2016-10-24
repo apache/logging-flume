57537dc29f8f06e145faae00695a6284db8e70fe
cjn082030 <wys372b@aliyun.com>
2016-10-24 16:56:10 +0800

    Update TestRegexLineDeserializer.java

--- flume-ng-core/src/test/java/org/apache/flume/serialization/TestRegexLineDeserializer.java
+++ flume-ng-core/src/test/java/org/apache/flume/serialization/TestRegexLineDeserializer.java
@@ -31,44 +31,43 @@ public class TestRegexLineDeserializer {
   @Before
   public void setup() {
     StringBuilder sb = new StringBuilder();
-    sb.append("2016-10-19 10:19:10,188 INFO  [lifecycleSupervisor-1-0] (org.apache.flume.node.PollingPropertiesFileConfigurationProvider.start:61)  - Configuration provider starting");
+    sb.append("2016-10-19 10:19:10,188 INFO  - Configuration provider starting");
     sb.append("\n");
-    sb.append("2016-10-19 10:19:10,199 DEBUG [conf-file-poller-0] (org.apache.flume.conf.FlumeConfiguration$AgentConfiguration.isValid:314)  - Starting validation of configuration for agent: agent, initial-configuration: AgentConfiguration[agent]");
+    sb.append("2016-10-19 10:19:10,199 DEBUG - Starting validation of for agent: agent");
     sb.append("\n");
-    sb.append("SOURCES: {s1={ parameters:{decodeErrorPolicy=REPLACE, fileHeader=true, interceptors.i1.type=host, interceptors.i2.type=timestamp, channels=c1, spoolDir=filedir, type=spooldir, fileDelay=10000, deserializer=org.apache.flume.serialization.RegexLineDeserializer$Builder, fileSuffix=.COMPLETED, deletePolicy=immediate, interceptors=i1 i2, basenameHeader=true, ignorePattern=^(.)*\\.tmp$} }}");
+    sb.append("SOURCES: {s1={ parameters:{decodeErrorPolicy=REPLACE} }}");
     sb.append("\n");
-    sb.append("CHANNELS: {c1={ parameters:{checkpointDir=./agent/checkpoint, dataDirs=./agent/data, transactionCapacity=10000, capacity=1000000, type=file} }}");
+    sb.append("CHANNELS: {c1={ parameters:{checkpointDir=./agent/checkpoint} }}");
     sb.append("\n");
-    sb.append("SINKS: {k1={ parameters:{clusterName=es, indexType=test4, serializer=org.apache.flume.sink.elasticsearch.ElasticSearchDynamicSerializer, indexName=test, batchSize=100, hostNames=127.0.0.1:9300,127.0.0.2:9300, type=elasticsearch, channel=c1} }}");
+    sb.append("SINKS: {k1={ parameters:{clusterName=es, indexType=test4} }}");
     sb.append("\n");
     sb.append("\n");
-    sb.append("2016-10-19 10:19:10,204 DEBUG [conf-file-poller-0] (org.apache.flume.conf.FlumeConfiguration$AgentConfiguration.validateChannels:469)  - Created channel c1");
+    sb.append("2016-10-19 10:19:10,204 DEBUG - Created channel c1");
     sb.append("\n");
     fileInfo = sb.toString();
   }
   
   @Test
   public void testMultiLine() throws IOException {
-      // ^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3}
-      String dd="^\\";
-      // ^((${srcStartWithString})(\\s|\\S)).*
-      Context context = new Context();
-      context.put("filePattern", "^((\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2},\\d{3})(\\s|\\S)).*");
-      ResettableInputStream in = new TestRegexLineInputStream(fileInfo);
-      EventDeserializer des = new RegexLineDeserializer(context, in);
-      Event evt = des.readEvent();
-      String line1 = new String(evt.getBody(), "UTF-8");
-      System.out.println("line 1\n" + line1);
-      des.mark();
-      evt = des.readEvent();
-      String line2 = new String(evt.getBody(), "UTF-8");
-      System.out.println("line 2\n" + line2);
-      des.mark();
-      evt = des.readEvent();
-      String line3 = new String(evt.getBody(), "UTF-8");
-      System.out.println("line 3\n" + line3);
-      des.reset();
-      des.mark();
-      des.close();
+    // ^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3}
+    // ^((${srcStartWithString})(\\s|\\S)).*
+    Context context = new Context();
+    context.put("filePattern", "^((\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2},\\d{3})(\\s|\\S)).*");
+    ResettableInputStream in = new TestRegexLineInputStream(fileInfo);
+    EventDeserializer des = new RegexLineDeserializer(context, in);
+    Event evt = des.readEvent();
+    String line1 = new String(evt.getBody(), "UTF-8");
+    System.out.println("line 1\n" + line1);
+    des.mark();
+    evt = des.readEvent();
+    String line2 = new String(evt.getBody(), "UTF-8");
+    System.out.println("line 2\n" + line2);
+    des.mark();
+    evt = des.readEvent();
+    String line3 = new String(evt.getBody(), "UTF-8");
+    System.out.println("line 3\n" + line3);
+    des.reset();
+    des.mark();
+    des.close();
   }
 }
