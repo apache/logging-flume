273efda19389ee44dc572a6e42c186fa4b0d1d82
sa <sebastian.alfesr@plista.com>
2017-01-03 15:29:23 +0100

    enum value for FlumeEventDeserializer

--- flume-ng-core/src/main/java/org/apache/flume/serialization/EventDeserializerType.java
+++ flume-ng-core/src/main/java/org/apache/flume/serialization/EventDeserializerType.java
@@ -26,6 +26,7 @@ import org.apache.flume.annotations.InterfaceStability;
 public enum EventDeserializerType {
   LINE(LineDeserializer.Builder.class),
   AVRO(AvroEventDeserializer.Builder.class),
+  FLUME(FlumeEventAvroEventDeserializer.Builder.class),
   OTHER(null);
 
   private final Class<? extends EventDeserializer.Builder> builderClass;
