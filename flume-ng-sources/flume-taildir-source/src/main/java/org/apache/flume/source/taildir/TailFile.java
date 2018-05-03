07c98978ec89c8119bbf833cd384b4ba59be8f10
lizhenhuan <zhenhuan.li@bkjk.com>
2018-05-03 12:45:53 +0800

    fix bug : Flume TAILDIR source lost last line when file upload from windows

--- flume-ng-sources/flume-taildir-source/src/main/java/org/apache/flume/source/taildir/TailFile.java
+++ flume-ng-sources/flume-taildir-source/src/main/java/org/apache/flume/source/taildir/TailFile.java
@@ -202,7 +202,10 @@ public class TailFile {
         }
       }
       for (int i = bufferPos; i < buffer.length; i++) {
-        if (buffer[i] == BYTE_NL) {
+        // linux file end with BYTE_NL,
+        // but windows file end with BYTE_NL , we can check filePointer == fileLength
+        // otherwise windows file may lost last end line
+        if (buffer[i] == BYTE_NL || (i == buffer.length - 1 && raf.getFilePointer() == raf.length())) {
           int oldLen = oldBuffer.length;
           // Don't copy last byte(NEW_LINE)
           int lineLen = i - bufferPos;
