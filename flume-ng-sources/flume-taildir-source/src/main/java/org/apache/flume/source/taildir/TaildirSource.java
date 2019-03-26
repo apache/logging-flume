6032128d2ee941890f48b0e63ccc89d5a286bd46
belvey <belvey@163.com>
2019-03-27 07:33:22 +0800

    Revert "FLUME-3325 fixed TaildirSource oom issue"
    This reverts commit 80325c6e0f6c906d063a680e4f3f780494a4c243.

--- flume-ng-sources/flume-taildir-source/src/main/java/org/apache/flume/source/taildir/TaildirSource.java
+++ flume-ng-sources/flume-taildir-source/src/main/java/org/apache/flume/source/taildir/TaildirSource.java
@@ -28,14 +28,11 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
-import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-
 
 import org.apache.flume.ChannelException;
 import org.apache.flume.Context;
@@ -85,7 +82,7 @@ public class TaildirSource extends AbstractSource implements
   private boolean cachePatternMatching;
 
   private List<Long> existingInodes = new CopyOnWriteArrayList<Long>();
-  private Set<Long> idleInodes = new CopyOnWriteArraySet<Long>();
+  private List<Long> idleInodes = new CopyOnWriteArrayList<Long>();
   private Long backoffSleepIncrement;
   private Long maxBackOffSleepInterval;
   private boolean fileHeader;
