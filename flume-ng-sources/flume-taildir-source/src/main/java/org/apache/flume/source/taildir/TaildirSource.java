054911bd146172454debc097c1a6021e8141b88b
felix-thinkingdata <felix@thinkingdata.cn>
2019-12-20 18:10:59 +0800

    If an exception occurs while FileWriter writing to the file, whereafter call FileWriter close method ,the file will be cleared. This is FileWriter characteristic. other model build failed

--- flume-ng-sources/flume-taildir-source/src/main/java/org/apache/flume/source/taildir/TaildirSource.java
+++ flume-ng-sources/flume-taildir-source/src/main/java/org/apache/flume/source/taildir/TaildirSource.java
@@ -342,18 +342,18 @@ public class TaildirSource extends AbstractSource implements
     }
   }
 
-    private void writePosition() {
-        File file = new File(positionFilePath);
-        try {
-            if (!existingInodes.isEmpty()) {
-                String json = toPosInfoJson();
-                FileUtils.write(file, json, "UTF-8", false);
-            }
-        } catch (Throwable t) {
-            logger.error("Failed writing positionFile", t);
-            sourceCounter.incrementGenericProcessingFail();
-        }
+  private void writePosition() {
+    File file = new File(positionFilePath);
+    try {
+      if (!existingInodes.isEmpty()) {
+        String json = toPosInfoJson();
+        FileUtils.write(file, json, "UTF-8", false);
+      }
+    } catch (Throwable t) {
+      logger.error("Failed writing positionFile", t);
+      sourceCounter.incrementGenericProcessingFail();
     }
+  }
 
   private String toPosInfoJson() {
     @SuppressWarnings("rawtypes")
