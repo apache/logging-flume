02bb8e6854c8e5d1850a07a7fe5e3338178d679f
Masatake Iwasaki <iwasakims@apache.org>
2020-02-26 11:36:43 +0900

    FLUME-3355 Upgrade Kafka to 2.3.1. (#316)

--- flume-ng-sources/flume-kafka-source/src/test/java/org/apache/flume/source/kafka/TestKafkaSource.java
+++ flume-ng-sources/flume-kafka-source/src/test/java/org/apache/flume/source/kafka/TestKafkaSource.java
@@ -881,7 +881,7 @@ public class TestKafkaSource {
     if (hasZookeeperOffsets) {
       KafkaZkClient zkClient = KafkaZkClient.apply(kafkaServer.getZkConnectString(),
               JaasUtils.isZkSecurityEnabled(), 30000, 30000, 10, Time.SYSTEM,
-              "kafka.server", "SessionExpireListener");
+              "kafka.server", "SessionExpireListener", scala.Option.empty());
       zkClient.getConsumerOffset(group, new TopicPartition(topic, 0));
       Long offset = tenthOffset + 1;
       zkClient.setOrCreateConsumerOffset(group, new TopicPartition(topic, 0), offset);
