7960f40ec13087d23c11e27f6f8e81d3764624bf
Chen768959 <934103231@qq.com>
2021-01-05 21:08:29 +0800

    Fix unusedImports

--- flume-ng-sources/flume-taildir-source/src/main/java/org/apache/flume/source/taildir/TaildirIncludeChildMatcher.java
+++ flume-ng-sources/flume-taildir-source/src/main/java/org/apache/flume/source/taildir/TaildirIncludeChildMatcher.java
@@ -30,8 +30,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.DirectoryStream;
