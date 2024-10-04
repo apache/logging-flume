d3e41aea7d9a24e54a7611d1b489072429b99de3
Piotr P. Karwasz <piotr.github@karwasz.org>
2024-10-04 22:39:25 +0200

    Make `VersionInfo` reproducible

--- flume-ng-core/src/main/java/org/apache/flume/tools/VersionInfo.java
+++ flume-ng-core/src/main/java/org/apache/flume/tools/VersionInfo.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,7 +17,12 @@
  */
 package org.apache.flume.tools;
 
-import org.apache.flume.VersionAnnotation;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.Properties;
 
 /*
  * This class provides version info of Flume NG
@@ -25,72 +30,70 @@ import org.apache.flume.VersionAnnotation;
 
 public class VersionInfo {
 
-  private static Package myPackage;
-  private static VersionAnnotation version;
+  private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss z yyyy");
+  private static final Properties properties = new Properties();
 
   static {
-    myPackage = VersionAnnotation.class.getPackage();
-    version = myPackage.getAnnotation(VersionAnnotation.class);
-  }
-
-  /**
-   * Get the meta-data for the Flume package.
-   * @return
-   */
-  static Package getPackage() {
-    return myPackage;
+    try (InputStream is = VersionInfo.class.getResourceAsStream("VersionInfo.properties");) {
+      properties.load(is);
+    } catch (IOException e) {
+      throw new IllegalStateException(e);
+    }
   }
 
   /**
    * Get the Flume version.
+   *
    * @return the Flume version string, eg. "1.1"
    */
   public static String getVersion() {
-    return version != null ? version.version() : "Unknown";
+    return properties.getProperty("version", "Unknown");
   }
 
   /**
    * Get the subversion revision number for the root directory
+   *
    * @return the revision number, eg. "100755"
    */
   public static String getRevision() {
-    if (version != null
-        && version.revision() != null
-        && !version.revision().isEmpty()) {
-      return version.revision();
-    }
-    return "Unknown";
+    return properties.getProperty("revision", "Unknown");
   }
 
   /**
    * Get the branch on which this originated.
+   *
    * @return The branch name, e.g. "trunk" or "branches/branch-1.1"
    */
   public static String getBranch() {
-    return version != null ? version.branch() : "Unknown";
+    return properties.getProperty("branch", "Unknown");
   }
 
   /**
    * The date that Flume was compiled.
+   *
    * @return the compilation date in unix date format
    */
   public static String getDate() {
-    return version != null ? version.date() : "Unknown";
+    String timestamp = properties.getProperty("date");
+    return timestamp == null
+        ? "Unknown"
+        : Instant.parse(timestamp).atZone(ZoneOffset.UTC).format(formatter);
   }
 
   /**
    * The user that compiled Flume.
+   *
    * @return the username of the user
    */
   public static String getUser() {
-    return version != null ? version.user() : "Unknown";
+    return properties.getProperty("user", "Unknown");
   }
 
   /**
    * Get the subversion URL for the root Flume directory.
    */
   public static String getUrl() {
-    return version != null ? version.url() : "Unknown";
+    return properties.getProperty("url", "Unknown");
   }
 
   /**
@@ -98,7 +101,7 @@ public class VersionInfo {
    * built.
    **/
   public static String getSrcChecksum() {
-    return version != null ? version.srcChecksum() : "Unknown";
+    return properties.getProperty("srcChecksum", "Unknown");
   }
 
   /**
